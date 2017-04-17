bc99f60496c15e4888dd58c73eef2a76a562d2de
joelcawthorne <joelcawthorne@Joels-MacBook-Pro.local>
2017-04-16 20:17:07 -0400

    Initial commit

---
+++ src/EmployeeLoginView.java
@@ -0,0 +1,7 @@
+public class EmployeeLoginView extends LoginView {
+   private static final long serialVersionUID = 1L;
+
+   EmployeeLoginView() { 
+      this.setUserLabel("Employee ID");
+   }
+}
