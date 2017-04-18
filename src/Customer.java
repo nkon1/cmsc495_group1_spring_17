c10c2c61401685cf5492347a5dc1a07a229e3c42
djamphi <djamphi@yahoo.com>
2017-04-18 21:59:12 +0200

    Update Customer.java

--- src/Customer.java
+++ src/Customer.java
@@ -3,6 +3,10 @@ public class Customer {
    private String firstName;
    private String lastName;
    private Address address;
+   private String email;
+   
+   private boolean newCustomer;  //was identified as newUser in the Pseudocode
+   private boolean existingCustomer; //was identified as existingUser in the Pseudocode
    
    Customer(String firstName, String lastName, Address address) {
       this.firstName = firstName;
@@ -22,7 +26,12 @@ public class Customer {
       this.address = address;
    }
    
+   public void setEmail(String email) {
+      this.email = email;
+   }  //added from Pseudocode by Stefan
+   
    public String getFirstName() { return firstName; }
    public String getLastName() { return lastName; }
    public Address getAddresss() { return address; }
+   public String getEmail() { return email; }
 }
