804199f7b80bc2fafcaf245d2d67ca4b2856a07b
joelcawthorne <joelcawthorne@10.0.1.54>
2017-04-18 21:31:54 -0400

    Added new customer button functionality and linked it to login screen. added functionality to cancel new customer, which takes a user back to login screen.

--- src/CustomerLoginView.java
+++
@@ -1,7 +0,0 @@
-public class CustomerLoginView extends LoginView {
-   private static final long serialVersionUID = -4679238664215211066L;
-
-   CustomerLoginView() {
-      this.setUserLabel("Email");
-   }
-}
